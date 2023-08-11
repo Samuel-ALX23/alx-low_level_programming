1232129618822736d24e7ae6ccb78cc557ac65c6
Samuel-ALX23 <ayitey_samuel@hotmail.com>
2023-08-11 11:11:43 -0700

    script that assign a random number to the variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
-#include <stdlib.h>
-
-#include <time.h>
-
 /**
-* main-program entry point.
-* Return:0-no error,non zero value if error.
-**/
+ * main - display if the number is positive |  negative | zero
+ * Return: return 0 and exit the program
+ */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n >  0)
-	{
+	if (n > 0)
 		printf("%d is positive\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
 	else if (n == 0)
 		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 	return (0);
 }
