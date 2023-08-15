567937709ec8ffef2247eeac5f7899949bdf7d5a
Samuel-ALX23 <ayitey_samuel@hotmail.com>
2023-08-15 15:01:43 -0700

    script that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,46 +6,13 @@
  *  @n: the provided value by the user.
  *  Return: Always 0 (success)
  */
-
 void print_times_table(int n)
 {
-	int row;
-	int column;
-	int product;
-
-	if (n < 0 || n >= 15)
-		return;
-	for (row = 0; row <= n; row++)
+	if (n >= 0 && n <= 15)
 	{
-		for (column = 0; column <= n; column++)
+		for (int i = 0; i <= 10; i++)
 		{
-			product = (row * column);
-			if (column == 0)
-				_putchar('0' + product);
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				if (product <= 9)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + product);
-				}
-				else if (product > 9 && product < 100)
-				{
-					_putchar(' ');
-					_putchar('0' + (product / 10));
-					_putchar('0' + (product % 10));
-				}
-				else if (product >= 100)
-				{
-					_putchar('0' + (product / 100));
-					_putchar('0' + ((product / 10) % 10));
-					_putchar('0' + (product % 10));
-				}
-			}
+			printf("%2d x %2d = %2d\n", n, i, n * i);
 		}
-		_putchar('\n');
 	}
 }
