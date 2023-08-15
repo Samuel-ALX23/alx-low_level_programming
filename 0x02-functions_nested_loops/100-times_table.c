a334c493f569e06f31676b265e97b39b9e102506
Samuel-ALX23 <ayitey_samuel@hotmail.com>
2023-08-15 15:55:37 -0700

    script that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,18 +1,48 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- *  print_times_table - prints time table with provided number
- *  @n: the provided value by the user.
- *  Return: Always 0 (success)
+ * print_times_table - Print n times table starting with 0
+ * Description: If n is greater than 15 or less than 0 print nothing
+ * @n: type number
  */
 void print_times_table(int n)
 {
-	if (n >= 0 && n <= 15)
+	int nu1 = 0, rw, cl;
+
+	if (n > 15 || n < 0)
+		return;
+	while (nu1 <= n)
 	{
-		for (int i = 0; i <= 10; i++)
+		for (rw = 0; rw <= n; rw++)
 		{
-			printf("%2d x %2d = %2d\n", n, i, n * i);
+			cl = nu1 * rw;
+			if (cl > 99)
+			{
+				_putchar(cl / 100 + '0');
+				_putchar((cl / 10 % 10) + '0');
+				_putchar(cl % 10 + '0');
+			}
+			else if (cl > 9)
+			{
+				_putchar(' ');
+				_putchar(cl / 10 + '0');
+				_putchar(cl % 10 + '0');
+			}
+			else if (rw != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(cl + '0');
+			}
+			else
+				_putchar(cl + '0');
+
+			if (rw != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
+		nu1++;
 	}
 }
