59f8bb5b14ce6907ed37eeccb341d3b861a93996
Samuel-ALX23 <ayitey_samuel@hotmail.com>
2023-09-01 08:17:26 -0700

    Firts Commit

---
+++ 0x09-static_libraries/obj-file/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry to goal
+ * description: best hacker
+ * Return: 0 always(success)
+ */
+
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}
