021b5f29004ac2f9787b16c379e865d81c954e4d
Samuel-ALX23 <ayitey_samuel@hotmail.com>
2023-09-13 13:10:42 -0700

    error correction

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,11 +1,13 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include <stddef.h>
 
 /**
- * get_op_func - func that selects correct op_ to make operation
- * @s: Type char str
- * Return: pointer to op_ of a and b return c or null if not operation
+ * get_op_func - function pointer that selects
+ * the correct function to perform
+ * @s: the operator given by the user
+ *
+ * Return: pointer to the function that corresponds to the
+ * operator given as a parameter
  */
 
 int (*get_op_func(char *s))(int, int)
@@ -20,13 +22,10 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-
 	i = 0;
-	while (ops[i].op)
-	{
-		if (ops[i].op[0] == s[0] && s[1] == '\0')
-			return (ops[i].f);
-		++i;
-	}
-	return (NULL);
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[i].f);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,10 +1,10 @@
 #include "3-calc.h"
 
 /**
- * op_add - sum of a and b
- * @a: first operand
- * @b: second operand
- * Return: a + b
+ * op_add - calculates the sum of two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: sum of a and b
  */
 
 int op_add(int a, int b)
@@ -13,10 +13,10 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - substract of a and b
- * @a: first operand
- * @b: second operand
- * Return: a - b
+ * op_sub - calculates the difference of two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: difference of a and b
  */
 
 int op_sub(int a, int b)
@@ -25,10 +25,10 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - multiply of a and b
- * @a: first operand
- * @b: second operand
- * Return: a * b
+ * op_mul - calculates the product of two integers
+ *  @a: 1st integer
+ *  @b: 2nd integer
+ *  Return: product of a and b
  */
 
 int op_mul(int a, int b)
@@ -37,10 +37,10 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - divide of a and b
- * @a: first operand
- * @b: second operand
- * Return: a / b
+ * op_div - calculates the division of two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: result of the division of a and b
  */
 
 int op_div(int a, int b)
@@ -49,10 +49,10 @@ int op_div(int a, int b)
 }
 
 /**
- * op_mod - module of a and b
- * @a: first operand
- * @b: second operand
- * Return: a % b
+ * op_mod - calculates the remainder of the division of two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: remainder of a divided b
  */
 
 int op_mod(int a, int b)
