2c07532280aca133d81b9465f74d312cd484a0c9
Samuel-ALX23 <ayitey.samuel@gmail.com>
2023-09-22 18:20:50 -0700

    [Task 5] a function that prints You're beat! and yet, you must allow before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * first - Prints a message to the console
+ * The purpose of this function is to display a message
+ **/
+
+void first(void)
+{
+	printf("you're best! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
