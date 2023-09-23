c9c0ec48394598ab4743033a81049d39f12ba2f4
Samuel-ALX23 <ayitey.samuel@gmail.com>
2023-09-22 18:42:39 -0700

    updated 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,10 @@
-#include "lists.h"
-
+#include <stdio.h>
 /**
- * first - Prints a message to the console
- * The purpose of this function is to display a message
- **/
-
-void first(void)
+ * bmain - function executed before main
+ * Return: no return.
+ */
+void __attribute__((constructor)) bmain()
 {
-	printf("you're best! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
