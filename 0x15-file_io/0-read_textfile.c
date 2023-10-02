6a8e67e619ecc6608669c90449f2634f531d86be
Samuel-ALX23 <ayitey.samuel@gmail.com>
2023-10-02 08:56:21 -0700

    a function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,37 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a textfile and prints the 
- * contents to the POSIX STDOUT
- * @filename: The number of command-line arguments
- * @letters: An array of strings containing the command-line
- * arguments to the STDOUT
- *
- * Return: Returns the number of characters printed
+ * read_textfile - reads a text file and prints the letter
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ * Return: numbers of letters printed. It fails, returns 0.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fild = 0, chk = 0;
-	char *buffer;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (!filename || !letters)
+	if (!filename)
 		return (0);
 
-	fild = open(filename, O_RDONLY);
-	if (fild < 0)
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * (letters));
-	if (!buffer)
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
 
-	chk = read(fild, buffer, letters);
-	chk = write(STDOUT_FILENO, buffer, chk);
-	if (chk < 0)
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	if (nwr == -1)
 		return (0);
 
-	close(fild);
-	free(buffer);
-
-	return (chk);
+	close(fd);
+	free(buf);
+	return (nwr);
 }
