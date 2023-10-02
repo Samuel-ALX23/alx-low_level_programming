f6db92d49681a6ac6aa2dafa60e8bba78e1fbb4d
Samuel-ALX23 <ayitey.samuel@gmail.com>
2023-10-02 13:05:34 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,53 @@
-#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints the letter
- * @filename: filename.
- * @letters: numbers of letters printed.
- * Return: numbers of letters printed. It fails, returns 0.
+ * read_textfile - Reads and prints a text file to standard output
+ * @filename: The name of the file to read
+ * @letters: The number of letters to read and print
+ *
+ * Return: The actual number of letters read and printed,
+ *         or 0 if an error occurred
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	int file_descriptor;
+	char *content_buffer;
+	ssize_t bytes_read, bytes_written;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	content_buffer = (char *)malloc(sizeof(char) * (letters + 1));
+	if (content_buffer == NULL)
+	{
+		close(file_descriptor);
+		return (0);
+	}
+
+	bytes_read = read(file_descriptor, content_buffer, letters);
+
+	if (bytes_read == -1)
+	{
+		free(content_buffer);
+		close(file_descriptor);
+		return (0);
+	}
+
+	content_buffer[bytes_read] = '\0';
+
+	bytes_written = write(STDOUT_FILENO, content_buffer, bytes_read);
+
+	free(content_buffer);
+	close(file_descriptor);
+
+	if (bytes_written != bytes_read)
 		return (0);
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
-	if (nwr == -1)
-		return (0);
-
-	close(fd);
-	free(buf);
-	return (nwr);
+	return (bytes_read);
 }
