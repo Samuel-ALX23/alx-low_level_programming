f10903c7ba926a6a73b67351762a2cabca2d80c1
Samuel-ALX23 <ayitey.samuel@gmail.com>
2023-10-02 13:27:37 -0700

    golden update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,53 +1,39 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * read_textfile - Reads and prints a text file to standard output
- * @filename: The name of the file to read
- * @letters: The number of letters to read and print
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
  *
- * Return: The actual number of letters read and printed,
- *         or 0 if an error occurred
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor;
-	char *content_buffer;
-	ssize_t bytes_read, bytes_written;
+	int fd;
+	ssize_t lenr, lenw;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-
-	content_buffer = (char *)malloc(sizeof(char) * (letters + 1));
-	if (content_buffer == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
-		close(file_descriptor);
+		close(fd);
 		return (0);
 	}
-
-	bytes_read = read(file_descriptor, content_buffer, letters);
-
-	if (bytes_read == -1)
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
 	{
-		free(content_buffer);
-		close(file_descriptor);
+		free(buffer);
 		return (0);
 	}
-
-	content_buffer[bytes_read] = '\0';
-
-	bytes_written = write(STDOUT_FILENO, content_buffer, bytes_read);
-
-	free(content_buffer);
-	close(file_descriptor);
-
-	if (bytes_written != bytes_read)
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
 		return (0);
-
-	return (bytes_read);
+	return (lenw);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,11 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
